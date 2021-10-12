ee23b2610465698def36311d5eac4f7151fb63c7
Riley Radle <61852965+19radleriley@users.noreply.github.com>
2021-10-12 12:04:21 -0500

    Redid the HTML code to be valid based on what I have learned in Web Development so far.  I will continue to update this as I learn more and more

--- animations.js
+++ animations.js
@@ -1,6 +1,6 @@
-const inner = document.querySelector(".inner");
-const text = document.querySelector(".text");
+// const inner = document.querySelector(".inner");
+// const text = document.querySelector(".text");
 
-const t1 = new TimelineMax();
-// t1.fromTo(inner, 1, {width: "0%"}, {width: "100%"});
-t1.fromTo(text, 5, {width: "0%"}, {width: "100%"});
+// const t1 = new TimelineMax();
+// // t1.fromTo(inner, 1, {width: "0%"}, {width: "100%"});
+// t1.fromTo(text, 5, {width: "0%"}, {width: "100%"});
